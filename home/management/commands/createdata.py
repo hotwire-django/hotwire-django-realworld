683e6b562593601fb78c6decc2309ab429ada999
Bo Lopker <lopkerk@gmail.com>
2021-01-06 10:43:42 -0800

    Clean slugs

--- home/management/commands/createdata.py
+++ home/management/commands/createdata.py
@@ -4,6 +4,7 @@ from random_username.generate import generate_username
 from essential_generators import DocumentGenerator
 from articles.models import Article, Comment, Tag
 from profiles.models import Profile
+import string
 import random
 
 
@@ -27,23 +28,29 @@ tags = [
 ]
 
 
+def clean_slug(slug):
+    allowed = string.ascii_lowercase + "-"
+    return "".join([a for a in slug if a in allowed])
+
+
 class Command(BaseCommand):
-    help = "Generates users, posts and comments."
-    article_upper_bound = 2
-    follows_upper_bounds = 2
-    favorties_upper_bounds = 2
+    help = "Generates users, articles, tag, follows and comments."
+    article_upper_bound = 5
+    follows_upper_bounds = 10
+    favorties_upper_bounds = 10
+    password = "turbodjango"
 
     def add_arguments(self, parser):
         parser.add_argument("num_users", type=int)
 
     def handle(self, *args, **options):
-        tag_objects = Tag.objects.bulk_create(
+        Tag.objects.bulk_create(
             [Tag(tag=t[0], slug=t[1]) for t in tags], ignore_conflicts=True
         )
         names = generate_username(int(options["num_users"]))
         User = get_user_model()
         users = User.objects.bulk_create(
-            [User(username=n, password="turbodjango") for n in names]
+            [User(username=n, password=self.password) for n in names]
         )
         print(users)
         gen = DocumentGenerator()
@@ -55,28 +62,18 @@ class Command(BaseCommand):
             articles = Article.objects.bulk_create(
                 [
                     Article(
-                        slug=gen.slug().replace(".", ""),
+                        slug=clean_slug(gen.slug()),
                         title=gen.sentence(),
                         description=gen.sentence(),
                         body=gen.paragraph(),
                         author=profile,
                     )
+                    # Make sure every user has at least 1 article
                     for _ in range(random.randrange(1, self.article_upper_bound))
                 ]
             )
             print(articles)
+            # Make sure every article has 1 tag, could add more later
             for article in articles:
                 article.tags.add(Tag.objects.get(slug=random.choice(tags)[1]))
-
-        # for poll_id in options["poll_ids"]:
-        #     try:
-        #         poll = Poll.objects.get(pk=poll_id)
-        #     except Poll.DoesNotExist:
-        #         raise CommandError('Poll "%s" does not exist' % poll_id)
-
-        #     poll.opened = False
-        #     poll.save()
-
-        #     self.stdout.write(
-        #         self.style.SUCCESS('Successfully closed poll "%s"' % poll_id)
-        #     )
+        self.stdout.write(self.style.SUCCESS(f"Created {len(users)} users"))
