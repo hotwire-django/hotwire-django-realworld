0c3de82f9485de713dcbddcf9d8c7765879d5da7
Bo Lopker <lopkerk@gmail.com>
2021-01-13 23:08:58 -0800

    Fix #8: allow users to favorite articles

--- realworld/settings.py
+++ realworld/settings.py
@@ -117,12 +117,11 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = []
+# STATICFILES_DIRS = ["assets"]
 
 STATIC_ROOT = BASE_DIR / "assets"
 
